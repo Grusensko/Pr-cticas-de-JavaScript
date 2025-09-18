3d478226bf0296b229bd5535e1bb1370b9845317
Grusensko <29048915+Grusensko@users.noreply.github.com>
2025-09-18 10:57:27 -0300

    switch con la variable dai

--- Curso JS Casa del Futuro/script.js
+++ Curso JS Casa del Futuro/script.js
@@ -62,6 +62,25 @@
         console.log("Es menor de edad");
     }
 
+    dia = 3;
+
+    switch (dia) {
+        case 1:
+            console.log("Lunes");  break;
+        case 2:
+            console.log("Martes"); break;
+        case 3:
+            console.log("Miércoles"); break;
+        case 4:
+            console.log("Jueves"); break;
+        case 5: 
+            console.log("Viernes"); break;
+        case 6:
+            console.log("Sábado"); break;   
+        case 7:
+            console.log("Domingo"); break;     
+    }
+
 
     console.log("-------------  CLASE 2 - JavaScript ------------------");
 
@@ -121,6 +140,11 @@ console.log("El resto de 17 ÷ 3 es: " + resto);
 // Declarar variable sin valor
 let sinValor;
 console.log("Valor inicial de sinValor: " + sinValor);
+
+
+
+
+
 
 
 
